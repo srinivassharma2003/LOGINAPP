5792f85b157babefb0ee3580f8a6c9179aaa41f0
unknown <kalagasrinivas2003@gmail.com>
2022-05-07 22:13:30 +0530

    Initial commit

---
+++ app/src/main/java/com/example/loginapp/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.loginapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    lateinit var email: EditText
+    lateinit var password: EditText
+    lateinit var login: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        email = findViewById(R.id.email)
+        password = findViewById(R.id.password)
+        login = findViewById(R.id.login)
+        login.setOnClickListener {
+            if(email.text.toString()=="abc@gmail.com" && password.text.toString()=="1234" ){
+                Toast.makeText(this,"logged successfully!",Toast.LENGTH_LONG).show()
+                startActivity(Intent(this,HomeScreen::class.java))
+            }
+            else{
+                Toast.makeText(this, "invalid credentials", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
